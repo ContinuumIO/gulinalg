cc0947d2e5d8b1b4a28b0fce17fc961c76648501
Oscar Villellas <oscar.villellas@continuum.io>
2015-02-24 12:11:37 +0100

    add build scripts for jenkins

---
+++ buildscripts/jenkins-build.bat
@@ -0,0 +1,22 @@
+REM This is the jenkins build script for building/testing the gulinalg
+REM module.
+REM
+REM In order to work, Jenkins should be configured such as:
+REM  - Anaconda is installed in C:\Anaconda
+REM  - Jenkins build matrix is used for multiple platforms/python
+REM    versions
+REM  - Use XShell plugin to launch this script (on Windows, the
+REM    .bat version will be executed instead)
+REM  - Call the script from the root workspace directory as
+REM    buildscripts/jenkins-build
+REM
+
+
+REM Require a version of python to be selected
+if "%PYTHON_VERSION%" == "" exit /b 1
+
+REM Require a version of NumPy to be selected
+if "%PYTHON_INTERPRETER%" == "" exit /b 1
+
+REM use conda-build to build/test the package.
+call C:\Anaconda\Scripts\conda build buildscripts/condarecipe --python %PYTHON_INTERPRETER% --numpy %NUMPY_VERSION%
