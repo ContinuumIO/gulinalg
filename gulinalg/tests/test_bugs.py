38232a1c10086c17f098554a842f6d9d70d020bc
Oscar Villellas <oscar.villellas@continuum.io>
2017-08-04 16:35:53 +0200

    added failing test reproducing matrix_multiply bug with a 0 dimension

--- gulinalg/tests/test_bugs.py
+++ gulinalg/tests/test_bugs.py
@@ -5,7 +5,7 @@ Tests that are reproductions of bugs found, to avoid regressions.
 from __future__ import print_function
 from unittest import TestCase
 import numpy as np
-from numpy.testing import run_module_suite
+from numpy.testing import run_module_suite, assert_allclose
 import gulinalg
 
 
@@ -19,7 +19,18 @@ class TestBugs(TestCase):
         gulinalg.matrix_multiply(a, b, out=c)
         assert not np.isnan(c).any()
 
+    def test_zero_dim_gemm(self):
+        a = np.zeros((2,0))
+        b = np.zeros((0,2))
+        c = np.empty((2,2))
+        c.fill(np.nan)
+        # make sure that the result is set to zero
+        gulinalg.matrix_multiply(a, b, out=c)
+        assert_allclose(c, np.zeros((2,2)))
+
+        # check that the correct shape is created as well...
+        d = gulinalg.matrix_multiply(a,b)
+        assert_allclose(d, np.zeros((2,2)))
 
 if __name__ == '__main__':
     run_module_suite()
-        
